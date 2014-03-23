2f49486f81a7d0862cff569a7dd94147fa7ed20c
Chris Cook <ccook5@cgs.rbksch.org>
2014-03-23 16:19:59 +0000

    Added setup.py for CX_Freeze

---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+ 
+exe = Executable(
+    script="countdown.pyw",
+    base="Win32GUI",
+    )
+ 
+setup(
+    name = "Countdown",
+    version = "0.4",
+    description = "Display a countdown clock that counts down to a specific time.",
+    executables = [exe]
+    )
